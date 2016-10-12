6800a825ff95bc05c5e02a00582fe0a253c1c138
Boris Yankov <borisyankov@gmail.com>
2016-10-12 16:58:08 +0300

    Fix <Direction /> unit test * Improve eslint * Add yarn.lock

--- src/graphical/__tests__/Direction-test.js
+++ src/graphical/__tests__/Direction-test.js
@@ -5,16 +5,16 @@ import Direction from '../Direction';
 describe('<Direction />', () => {
     it('renders without any properties', () => {
         const wrapper = shallow(<Direction />);
-        expect(wrapper.find('DirectionNone').length).toEqual(1);
+        expect(wrapper.html()).toContain('fill:grey');
     });
 
     it('renders when direction is negative', () => {
         const wrapper = shallow(<Direction diff={-123} />);
-        expect(wrapper.find('DirectionDown').length).toEqual(1);
+        expect(wrapper.html()).toContain('fill:red');
     });
 
     it('renders when direction is positive', () => {
         const wrapper = shallow(<Direction diff={123} />);
-        expect(wrapper.find('DirectionUp').length).toEqual(1);
+        expect(wrapper.html()).toContain('fill:green');
     });
 });
